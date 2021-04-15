e99cfe78c99aa23c305ebd73e4f72b6f8d190a61
Universal Omega <1010delta1010@gmail.com>
2021-04-15 18:04:17 +0000

    [PipeEscape] Cleanup and convert to ExtensionRegistry
    Bug: T214470
    Change-Id: I783c39ff250b085389edc212a2bf59d949e1163c

--- ExtPipeEsc.php
+++ ExtPipeEsc.php
@@ -1,23 +1,28 @@
 <?php
 
 class ExtPipeEsc {
-	private static $parserFunctions = array('!' => 'pipeChar');
+	private static $parserFunctions = [ '!' => 'pipeChar' ];
 
 	public static function setup( &$parser ) {
 		// register each hook
-		foreach( self::$parserFunctions as $hook => $function )
+		foreach ( self::$parserFunctions as $hook => $function ) {
 			$parser->setFunctionHook( $hook,
-				array( __CLASS__, $function ), Parser::SFH_OBJECT_ARGS );
+				[ __CLASS__, $function ], Parser::SFH_OBJECT_ARGS );
+		}
+
 		return true;
 	}
 
 	public static function pipeChar( &$parser, $frame, $args ) {
 		$output = array_shift( $args );
 		// no parameters means we're done.  spit out an empty string
-		if ( !isset( $output ) )
+		if ( !isset( $output ) ) {
 			return '';
+		}
+
 		// expand the first argument
 		$output = $frame->expand( $output );
+
 		// get the rest of the arguments, expand each one, prefix each expansion
 		// with a pipe character, and append it to the output string.
 		for ( $arg = array_shift( $args );
@@ -26,7 +31,7 @@ class ExtPipeEsc {
 		{
 			$output .= '|' . $frame->expand( $arg );
 		}
-		//return '<pre><nowiki>'. trim( $output ) . '</nowiki></pre>';
+
 		return trim( $output );
 	}
 }
